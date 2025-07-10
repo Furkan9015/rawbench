031bd5b4af991f4139d46b26afc17ec41f7c24f0
Furkan Eris <furkan6olm@gmail.com>
2025-07-10 04:42:43 +0200

    data download commands

--- pod5/download.sh
+++ pod5/download.sh
@@ -1,6 +1,17 @@
+## H. sapiens
+
 cd hsapiens
 aws s3 cp s3://ont-open-data/giab_2023.05/flowcells/hg002/20230424_1302_3H_PAO89685_2264ba8c/pod5_pass/2264ba8c_afee3a87_1.0_275.pod5 ./ --no-sign-request
 aws s3 cp s3://ont-open-data/giab_2023.05/flowcells/hg002/20230424_1302_3H_PAO89685_2264ba8c/pod5_pass/2264ba8c_afee3a87_26.0_418.pod5 ./ --no-sign-request
 aws s3 cp s3://ont-open-data/giab_2023.05/flowcells/hg002/20230424_1302_3H_PAO89685_2264ba8c/pod5_pass/2264ba8c_afee3a87_32.0_225.pod5 ./ --no-sign-request
 aws s3 cp s3://ont-open-data/giab_2023.05/flowcells/hg002/20230424_1302_3H_PAO89685_2264ba8c/pod5_pass/2264ba8c_afee3a87_9.0_207.pod5 ./ --no-sign-request
 
+
+## E. coli
+
+cd ../ecoli
+
+# pip install pod5
+pod5 convert fast5 -r -t 64 --output-one-to-one ./fast5_files/ ./fast5_files/ ./pod5_files/
+
+## D. melanogaster
